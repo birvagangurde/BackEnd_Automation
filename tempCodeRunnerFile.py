c99d285c7866353c02fea976a3f3aa8da5156872
birvagangurde <gangurdebirva@gmail.com>
2025-10-25 03:01:46 +0530

    building a logic to get the data

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -13,11 +13,3 @@ with open('utilities/loanapp.csv') as csvFile:
 
 print(names)
 print(stats)
-
-Index = names.index('Joe')
-loanStatus = stats[Index]
-print(' loan status is '+loanStatus)
-
-with open('utilities/loanapp.csv','a') as wFile:
-    write = csv.writer(wFile)
-    write.writerow(["Bob","rejected"])
